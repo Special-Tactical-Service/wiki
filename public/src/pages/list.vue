d829a4a9742794e880d843add62420b456fd1662
Marvin Blum <marvin@marvinblum.de>
2019-09-06 15:50:36 +0200

    Added articles to list page.

--- public/src/pages/list.vue
+++ public/src/pages/list.vue
@@ -1,15 +1,56 @@
 <template>
     <layout>
         <template>
-            <h1>Listenname</h1>
+            <h1>{{list.name.name}}</h1>
+            <div class="info">
+                {{list.name.info}} -
+                {{list.def_time | moment("ll")}} {{$t("created")}} -
+                {{list.mod_time | moment("ll")}} {{$t("edited")}}
+            </div>
+            <articlecard v-for="article in entries" :key="article.id" :article="article"></articlecard>
         </template>
     </layout>
 </template>
 
 <script>
 import layout from "../components/layout.vue";
+import articlecard from "../components/article-card.vue";
 
 export default {
-    components: {layout}
+    components: {layout, articlecard},
+    data() {
+        return {
+            list: {name: {}, def_time: new Date(), mod_time: new Date()},
+            entries: []
+        };
+    },
+    mounted() {
+        let id = this.$route.params.id;
+        this.loadList(id);
+        this.loadEntries(id);
+    },
+    methods: {
+        loadList(id) {
+            this.emvi.getList(id)
+            .then(list => {
+                this.list = list.list;
+            });
+        },
+        loadEntries(id) {
+            this.emvi.getListEntries(id)
+            .then(results => {
+                this.entries = results.entries;
+            });
+        }
+    }
 }
 </script>
+
+<i18n>
+{
+    "de": {
+        "edited": "zuletzt bearbeitet",
+        "created": "erstellt"
+    }
+}
+</i18n>
